14d0621e2a83aa23d7e107c336fdc6406a88a85a
Orem Gitonga <62249915+orem-m-g@users.noreply.github.com>
2021-09-20 00:11:17 +0300

    Added HTML, CSS and JavaScript files
    Simple Calculator

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// function clears all the values
+function clearScreen() {
+ document.getElementById("result").value = "";
+}
+// function displays values
+function display(value) {
+ document.getElementById("result").value += value;
+}
+// function evaluates the expression and returns result
+function calculate() {
+ var p = document.getElementById("result").value;
+ var q = eval(p);
+ document.getElementById("result").value = q;
+}
